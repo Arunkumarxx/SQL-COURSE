56b8ca2dc269731de61daeda9525419b04a6d7c1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-23 02:33:21 +0530

    modified

--- SQL SELECT/SQL_SELECT.sql
+++ SQL SELECT/SQL_SELECT.sql
@@ -1,2 +1,8 @@
-﻿-- SQL script for SQL SELECT
--- Add your SQL commands here
+﻿USE personal_database;
+
+
+INSERT INTO StudentInfo (StudentID, StudentName, Age)
+VALUES (10, 'Arunkumar', 19);
+
+select  * from personal_database.studentinfo
+order by StudentID desc
