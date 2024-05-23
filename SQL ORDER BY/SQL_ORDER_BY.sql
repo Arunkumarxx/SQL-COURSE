6d5e913a12f074bef911d622d18051efd5beccc1
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-23 13:12:23 +0530

    modified

--- SQL ORDER BY/SQL_ORDER_BY.sql
+++ SQL ORDER BY/SQL_ORDER_BY.sql
@@ -1,2 +1,11 @@
-﻿-- SQL script for SQL ORDER BY
--- Add your SQL commands here
+﻿# The ORDER BY keyword is used to sort
+# the result-set in ascending or descending order.
+use personal_database;
+
+select  * from personal_database.studentinfo
+
+order by StudentID;
+
+# SELECT column1, column2, ...
+# FROM table_name
+# ORDER BY column1, column2, ... ASC|DESC;
