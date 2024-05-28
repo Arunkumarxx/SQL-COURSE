65aad02b4ec3a70f3e17024649743f1688f4721e
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-28 22:27:04 +0530

    modified

--- SQL FULL JOIN/SQL_FULL_JOIN.sql
+++ SQL FULL JOIN/SQL_FULL_JOIN.sql
@@ -1,2 +1,10 @@
-﻿-- SQL script for SQL FULL JOIN
--- Add your SQL commands here
+﻿# SQL FULL OUTER JOIN Keyword
+
+# SQL FULL OUTER JOIN Keyword
+# The FULL OUTER JOIN keyword returns all records
+# when there is a match in left (table1) or right (table2) table records.
+#
+# Tip: FULL OUTER JOIN and FULL JOIN are the same.
+
+select customers.CustomerName,shippers.ShipperName
+from customers
