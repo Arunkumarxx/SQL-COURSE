fcfd16d69d479674e1e5e76ccec75778d79168d2
Arunkumarxx <arunkumarthirumalaisamy2004@gmail.com>
2024-05-29 20:18:23 +0530

    modified

--- SQL NULL FUNCTIONS/SQL_NULL_FUNCTIONS.sql
+++ SQL NULL FUNCTIONS/SQL_NULL_FUNCTIONS.sql
@@ -1,2 +1,13 @@
-﻿-- SQL script for SQL NULL FUNCTIONS
--- Add your SQL commands here
+﻿# In MySQL, you can use the IFNULL() function to handle NULL values.
+# It returns the first non-NULL expression among its arguments. The syntax for the IFNULL() function is:
+
+# sql
+# Copy code
+# IFNULL(expr1, expr2)
+# Here's what each part of the syntax means:
+
+# expr1: This is the expression that you want to check for NULL. If expr1 is not NULL, the IFNULL() function returns expr1.
+# expr2: This is the value that is returned if expr1 is NULL.
+# If expr1 is NULL, the IFNULL() function returns expr2.
+
+# Here's an example of how you can use the IFNULL() function in a SQL query:
